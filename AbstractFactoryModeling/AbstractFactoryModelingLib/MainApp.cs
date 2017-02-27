eb07d39448d98f9c91e78fdd2dcac4dc4b10d2b2
cvillanaranjo <camilo.villa@yuxiglobal.com>
2017-02-27 18:27:04 -0500

    Create an Abstract Factory Example

---
+++ AbstractFactoryModeling/AbstractFactoryModelingLib/MainApp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AbstractFactoryModelingLib
+{
+    class MainApp
+    {
+        public static void Main()
+        {
+            //Create and run the African animal world.
+            ContinentFactory africa = new AfricanFactory();
+            AnimalKingdom world = new AnimalKingdom(africa);
+
+            world.RunFoodChain();
+
+            //Create and run the American anmal world.
+            ContinentFactory america = new AmericaFactory();
+            world = new AnimalKingdom(america);
+            world.RunFoodChain();
+
+            //wait for user input
+            Console.ReadKey();
+
+        }
+    }
+}
